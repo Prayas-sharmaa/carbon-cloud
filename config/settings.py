7bf0dcc9482978f1c349750499472e04b8dce147
Prayas-sharmaa <x24280585@student.ncirl.ie>
2026-03-13 00:38:11 +0000

    sql config changed

--- config/settings.py
+++ config/settings.py
@@ -53,9 +53,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/app/data/db.sqlite3',
     }
 }
 
