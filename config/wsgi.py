a4848403717275825b98d991ea4eb27e81f80f2e
Prayas-sharmaa <x24280585@student.ncirl.ie>
2026-03-26 01:39:21 +0000

    Datadog Integration

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+# Datadog APM Integration 
+from ddtrace import patch_all, tracer
+
+# Automatically instrument all supported libraries (Django, DBs, HTTP clients, etc.)
+patch_all()
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
